8bd0975327a7a74e40a65449c7e1a5c13935a743
github username <github email address>
2018-09-27 13:43:40 -0400

    backend finished

---
+++ salty-slugs-backend/app/controllers/api/v1/scores_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::ScoresController < ApplicationController
+  before_action :find_score, only: [:update]
+  def index
+    @scores = Score.all
+    render json: @scores
+  end
+
+  def update
+    @note.update(score_params)
+    if @score.save
+      render json: @score, status: :accepted
+    else
+      render json: { errors: @score.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def score_params
+    params.permit(:title, :content)
+  end
+
+  def find_score
+    @score = Score.find(params[:id])
+  end
+end
